d18b1fc47d9929889d24f53a80b375ff31972c40
Lorent Lempereur <lorent.lempereur@n8n.io>
2026-09-22 12:11:35 +0200

    refactor(core): Rename RetiredOccurrence to RetiredTask

--- packages/@n8n/db/src/repositories/index.ts
+++ packages/@n8n/db/src/repositories/index.ts
@@ -70,7 +70,7 @@ export type {
 	HostedClaimedRef,
 	DeleteFinishedTasksOptions,
 	RetireMissedResult,
-	RetiredOccurrence,
+	RetiredTask,
 	ScheduledTaskMetricSnapshot,
 } from './scheduled-task.repository';
 export {
